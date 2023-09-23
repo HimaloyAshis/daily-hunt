fdde7857dfa4658790f1145da209e64661b53bec
himaloyash <himaloyashis98@gmail.com>
2023-09-23 13:25:53 +0600

    worker done

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-console.log('ok')
+const worker = new Worker('worker.js')
 
 const background = document.getElementById('bg')
+const inTotal = document.getElementById('total')
+
+
+inTotal.addEventListener('click', ()=>{
+
+    worker.postMessage('worker please work for me')
+
+    // console.log(`total ${total}`)
+})
+
+
+
+background.addEventListener("click", ()=>{
+    
+    if(document.body.style.background !== 'green'){
+        document.body.style.background = 'green'
+    }else{
+        document.body.style.background = 'blue'
+    }
+})
---
+++ worker.js
@@ -0,0 +1,10 @@
+onmessage= (message)=>{
+
+    let total = 0;
+    for (let i = 0; i < 10000000000; i++) {
+        total += i;
+        
+    }
+
+    postMessage(total)
+}
