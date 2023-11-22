35066bd358b8b2b9a2082eec6d96389d5517e455
himaloyash <himaloyashis98@gmail.com>
2023-11-23 00:01:08 +0600

    for

--- object.js
+++ object.js
@@ -6,3 +6,12 @@ console.log(ok)
 
 const loveThePeopleYouMiss =[{maa:'Aroti', Baba:'poritosh', Didi:'shipuP(durga de)', vaidi:'sagor'}]
 console.log(loveThePeopleYouMiss)
+
+
+let bithy = 0
+
+for (let bithy = 0; bithy < 9; bithy++) {
+    const element = array[bithy];
+    console.log(element)
+    
+}
