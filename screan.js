f54bd0a8db7d81cd08016792ba44c5b931e7b4a3
himaloyash <himaloyashis98@gmail.com>
2024-02-14 00:17:01 +0600

    window manipulation

---
+++ screan.js
@@ -0,0 +1,6 @@
+const width = document.getElementById("width")
+const height = document.getElementById("height")
+
+
+width.innerHTML = "the window width is" + window.innerWidth
+height.innerHTML = "the window height is" + window.innerHeight
