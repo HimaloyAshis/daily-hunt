6bd82861e4c0aafccf09e993c08e07a336d69657
himaloyash <himaloyashis98@gmail.com>
2024-02-15 12:41:28 +0600

    screen manipulation almost done

--- screan.js
+++ screan.js
@@ -1,6 +1,13 @@
-const width = document.getElementById("width")
-const height = document.getElementById("height")
+const width = document.getElementById("width");
+const height = document.getElementById("height");
+const availHeight = document.getElementById("availHeight");
+const availWidth = document.getElementById("availWidth");
+const colorDepth = document.getElementById("colorDepth");
+const PixelDepth = document.getElementById("PixelDepth");
 
 
-width.innerHTML = "the window width is" + window.innerWidth
-height.innerHTML = "the window height is" + window.innerHeight
+
+width.innerHTML = "the window width is " + screen.width
+height.innerHTML = "the window height is " + screen.height
+availWidth.innerHTML = "the window height is " + screen.availWidth
+colorDepth.innerHTML = "the window height is " + screen.colorDepth
